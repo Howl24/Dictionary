55d83ba30d1d9e43e8c355caa11f4e04cdd9b07c
Daniel Matos <daniel.matos@pucp.pe>
2017-08-04 08:01:31 -0500

    Add simple test cmd

--- load.py
+++ load.py
@@ -7,7 +7,17 @@ cmd = """
       INSERT INTO dictionary_phrases
       (dict_name, phrase, quantity, representative, source, state)
       VALUES
-      ('test', 'excel intermedio', 510, 'excel intermedio', 'new_btpucp', False);
+      ('test', 'excel intermedio', 300, 'excel intermedio', 'new_btpucp', False);
       """
+
+session.execute(cmd);
+
+cmd = """
+      INSERT INTO dictionary_phrases
+      (dict_name, phrase, quantity, representative, source, state)
+      VALUES
+      ('test', 'excel', 100, 'excel intermedio', 'new_btpucp', False);
+      """
+ 
 
 session.execute(cmd);
